57f0a509f0133db5e7801b8de023f8d49474297a
Guillaume Poulin <poulin.guillaume@gmail.com>
2013-09-07 22:59:54 +0800

    Correct bees_fit test and importation

--- beem/__init__.py
+++ beem/__init__.py
@@ -1,9 +1,3 @@
-"""
-Created on Sat Dec  8 15:19:09 2012
-
-@author: Guillaume Poulin
-"""
-
 def main():
     import beem.ui as ui
     ui.main()
--- beem/test/bees_fit.py
+++ beem/test/bees_fit.py
@@ -1,19 +1,16 @@
 # -*- coding: utf-8 -*-
-"""
-Created on Sat Jan  5 02:11:17 2013
-
-@author: Guillaume Poulin
-"""
 
 from beem.io import bees_from_file
-from beem.experiment import BEESFit
+from beem.experiment import BEESFit, use_pure_c
 import numpy as np
 
 def test_fit():
     a=bees_from_file('/disk/Dropbox/nus/master/BEEM/2012-12-26/BEEM_HfO-3_D3_001.dat')
     b=BEESFit(a[0])
-    b.update(b._auto_range_fit(auto_range=0.4,noise=0.1,barrier_height=[-0.8],trans_a=[0.1]))
-    print(b.barrier_height[0],b.trans_a[0],b.noise,np.sqrt(b.r_squared))
+    c=b._auto_range_fit(auto_range=0.4,noise=0.1,barrier_height=[-0.8],trans_a=[0.1])
+    use_pure_c(False)
+    d=b._auto_range_fit(auto_range=0.4,noise=0.1,barrier_height=[-0.8],trans_a=[0.1])
+    print(d['barrier_height'],c['barrier_height'])
 
     
 if __name__ == "__main__":
