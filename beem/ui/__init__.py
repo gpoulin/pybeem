5a23bf50df4c15b1f48e655f48af6be57bb1e422
Guillaume Poulin <poulin.guillaume@gmail.com>
2013-09-18 18:05:28 +0800

    Add grid2dplot

--- beem/ui/__init__.py
+++ beem/ui/__init__.py
@@ -4,4 +4,4 @@
 
 from .ui import load_pref, save_pref, open3ds, select_file, _pref, fast_analysis
 from .bees_selector import select_bees
-from .graph import cdfplot, contourplot, dualplot, normplot
+from .graph import cdfplot, contourplot, dualplot, normplot, grid2dplot
--- beem/ui/graph.py
+++ beem/ui/graph.py
@@ -4,6 +4,7 @@
 import numpy as np
 from scipy.special import erfinv
 import matplotlib.pyplot as mpl
+from beem.experiment.experiment import MODE
 
 def dualplot(bees,**kwds):
     bh = np.abs([x.barrier_height[0] for x in bees])
@@ -23,7 +24,7 @@ def contourplot(bees, bins=None ,range=None, bh_index=0,**kwds):
     else:
         mb=np.mean(bh)
         sb=np.std(bh)
-        r1=min(np.percentile(r,98),np.mean(r)+3*np.std(r))
+        r1=min(np.percentile(r,99.5),np.mean(r)+3*np.std(r))
         range=[[0,r1],[mb-3*sb,mb+3*sb]]
 
     if bins==None:
@@ -37,6 +38,34 @@ def contourplot(bees, bins=None ,range=None, bh_index=0,**kwds):
     mpl.xlabel('$\phi$ (eV)')
     mpl.ylabel('R (eV$^{-1}$)')
     return (CS1,c)
+
+def grid2dplot(grid,attribute='barrier_height', num=0, pass_num=1, sweep=1,
+               mode=MODE['fwd'],cmap='Blues',bad='k',under='g',over='r',
+               threshold=0.6, **kwd):
+    val=np.ones((grid.num_x,grid.num_y))*np.nan
+    for b in grid.beesfit:
+        if b.pass_number == pass_num and b.number == sweep and \
+                b.mode == mode and b.r_squared>=threshold:
+            tmp=b.__getattribute__(attribute)
+            if num!=-1:
+                tmp=tmp[num]
+            val[b.x_index,b.y_index]=tmp
+    if attribute=='barrier_height':
+        with np.errstate(invalid='ignore'):
+            val=np.abs(val)
+    b=np.ma.masked_invalid(val)
+    cmap=mpl.get_cmap(cmap)
+    cmap.set_bad(bad)
+    cmap.set_under(under)
+    cmap.set_over(over)
+    x=np.linspace(0,grid.size_x,grid.num_x)
+    y=np.linspace(0,grid.size_y,grid.num_y)
+    mpl.pcolormesh(x,y,b.T,cmap=cmap,**kwd)
+    mpl.colorbar(extend='both')
+    mpl.xlim([0,grid.size_x])
+    mpl.ylim([0,grid.size_y])
+    return val
+
 
 def normplot(x,*arg,**kwarg):
     y=erfinv(np.linspace(-1,1,len(x)+2)[1:-1:])
